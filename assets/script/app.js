276ce111a55b4ab178d3a43ba01b90548de7d1ca
Durjoy96 <mddurjoy853@gmail.com>
2024-08-18 23:26:52 +0600

    landing page complete

--- assets/script/app.js
+++ assets/script/app.js
@@ -26,7 +26,5 @@ document.addEventListener("DOMContentLoaded", () => {
         const imgSrc = ["image-1.png", "image-2.png", "image-3.png", "image-4.png", "image-5.png", "image-6.png", "image-7.png", "image-8.png", "image-9.png", "image-10.png"];
         let index = Math.floor(Math.random() * imgSrc.length);
         return imgSrc[index];
-    }
-
-
+    };
 });
